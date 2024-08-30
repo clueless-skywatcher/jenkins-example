dd9d5b57987ebf044b0a68b92c108f8cb43b6f82
clueless-skywatcher <somichat@gmail.com>
2024-08-30 21:17:10 +0530

    first commit

---
+++ src/main/java/com/github/cluelessskywatcher/jenkinsexample/controllers/HelloController.java
@@ -0,0 +1,17 @@
+package com.github.cluelessskywatcher.jenkinsexample.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/message")
+	public Map<String, Object> getMessage() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("message", "Hello World!");
+		return map;
+	}
+}
---
+++ src/test/java/com/github/cluelessskywatcher/jenkins_example/JenkinsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cluelessskywatcher.jenkins_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
